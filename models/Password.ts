eca9d7778e1e2b16155c073ca54525c4799b982a
surfacez <sergiobatistajr94@gmail.com>
2023-12-06 17:52:29 -0300

    Refactor password hashing in Password.ts

--- models/Password.ts
+++ models/Password.ts
@@ -2,9 +2,7 @@ import argon2 from "argon2";
 export default class Password {
   constructor() {}
   async hash(password: string) {
-    const hash = await argon2.hash(password, {
-      timeCost: 20,
-    });
+    const hash = await argon2.hash(password);
     return hash;
   }
   async verify(hash: string, password: string) {
