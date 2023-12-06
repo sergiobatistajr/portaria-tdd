e12607dd91de173b5abba10294038e2f793d72c7
surfacez <sergiobatistajr94@gmail.com>
2023-12-06 17:47:36 -0300

    Fix beforeAll hook in POST user test

--- test/integration/api/v1/users/post.test.ts
+++ test/integration/api/v1/users/post.test.ts
@@ -1,5 +1,5 @@
 describe("Api POST user test", () => {
-  afterAll(async () => {
+  beforeAll(async () => {
     const response = await fetch(`http://localhost:3000/api/v1/test`, {
       method: "DELETE",
     });
