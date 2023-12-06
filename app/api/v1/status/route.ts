505908510c54b6bd3ba629ff30fcc8d5cb9f8236
surfacez <sergiobatistajr94@gmail.com>
2023-12-06 13:52:42 -0300

    Refactor database import and queries

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,15 +1,13 @@
-import database from "infra/database";
+import sql from "infra/database";
 
 export async function GET() {
   const updateAt = new Date();
-  const databaseVersion = (await database.query("SHOW server_version;")).rows[0]
+  const databaseVersion = (await sql("SHOW server_version;")).rows[0]
     .server_version;
-  const databaseMaxConnections = (await database.query("SHOW max_connections;"))
-    .rows[0].max_connections;
+  const databaseMaxConnections = (await sql("SHOW max_connections;")).rows[0]
+    .max_connections;
   const databaseCurrentConnections = (
-    await database.query(
-      "SELECT count(*) FROM pg_stat_activity WHERE state = 'active';",
-    )
+    await sql("SELECT count(*) FROM pg_stat_activity WHERE state = 'active';")
   ).rows[0].count;
 
   return new Response(
