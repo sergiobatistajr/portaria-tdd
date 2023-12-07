66984f247db8e47b3a2c1387f1ee4c720188f3bd
surfacez <sergiobatistajr94@gmail.com>
2023-12-07 15:45:40 -0300

    Refactor userCreate function to use separate validators for input fields

--- models/validator.ts
+++ models/validator.ts
@@ -1,25 +1,33 @@
 import { z } from "zod";
 import { UserCreate } from "./definitions";
 
+const idValidator = z.string().uuid();
+const statusValidator = z.enum(["active", "deactivate"]);
+const nameValidator = z
+  .string()
+  .min(3, "Name is required")
+  .refine((name) => name.split(" ").length > 1, "Last name is required");
+const emailValidator = z.string().email("Email is invalid");
+const passwordValidator = z.string().min(8, "Password is weak");
+const confirmPasswordValidator = z.string().min(8, "Confirm password is weak");
+const roleValidator = z.enum(["admin", "user", "report"]);
+
 function userCreate(inputFields: UserCreate) {
-  const validIpuntFields = z
+  const validInputFields = z
     .object({
-      id: z.string().uuid(),
-      status: z.enum(["active", "deactivate"]),
-      name: z
-        .string()
-        .min(3, "Name is required")
-        .refine((name) => name.split(" ").length > 1, "Last name is required"),
-      email: z.string().email("Email is invalid"),
-      password: z.string().min(8, "Password is weak"),
-      confirmPassword: z.string().min(8, "Confirm password is weak"),
-      role: z.enum(["admin", "user", "report"]),
+      id: idValidator,
+      status: statusValidator,
+      name: nameValidator,
+      email: emailValidator,
+      password: passwordValidator,
+      confirmPassword: confirmPasswordValidator,
+      role: roleValidator,
     })
     .refine((data) => data.password === data.confirmPassword, {
       message: "Passwords do not match",
     })
     .safeParse(inputFields);
-  return validIpuntFields;
+  return validInputFields;
 }
 
 export default Object.freeze({
