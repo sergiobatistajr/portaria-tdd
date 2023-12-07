decf0ef1ce75ff94dd328f3ee4b04ec559023780
surfacez <sergiobatistajr94@gmail.com>
2023-12-07 15:10:41 -0300

    Refactor Password class to use functions instead of methods

--- models/Password.ts
+++ models/Password.ts
@@ -1,12 +1,15 @@
 import argon2 from "argon2";
-export default class Password {
-  constructor() {}
-  async hash(password: string) {
-    const hash = await argon2.hash(password);
-    return hash;
-  }
-  async verify(hash: string, password: string) {
-    const result = await argon2.verify(hash, password);
-    return result;
-  }
+
+async function hash(password: string) {
+  const hash = await argon2.hash(password);
+  return hash;
 }
+async function verify(hash: string, password: string) {
+  const result = await argon2.verify(hash, password);
+  return result;
+}
+
+export default Object.freeze({
+  hash,
+  verify,
+});
