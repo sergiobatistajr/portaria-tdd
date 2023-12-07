4bcf5ab0ec57b1c3069f7a6f95ed7bdddc2b1f67
surfacez <sergiobatistajr94@gmail.com>
2023-12-07 15:40:50 -0300

    Refactor status route to use status model

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,30 +1,28 @@
-import database from "infra/database";
+import status from "models/status";
 
 export async function GET() {
   const updateAt = new Date();
-  const databaseVersion = (await database.sql("SHOW server_version;")).rows[0]
-    .server_version;
-  const databaseMaxConnections = (await database.sql("SHOW max_connections;"))
-    .rows[0].max_connections;
-  const databaseCurrentConnections = (
-    await database.sql(
-      "SELECT count(*) FROM pg_stat_activity WHERE state = 'active';",
-    )
-  ).rows[0].count;
-
-  return new Response(
-    JSON.stringify({
-      update_at: updateAt,
-      dependecies: {
-        database: {
-          version: databaseVersion,
-          max_connections: databaseMaxConnections,
-          current_connections: databaseCurrentConnections,
-        },
-      },
-    }),
-    {
-      status: 200,
+  const databaseStatus = await getDatabaseStatus();
+  const systemStatus = {
+    updated_at: updateAt,
+    dependecies: {
+      database: databaseStatus,
     },
-  );
+  };
+  return new Response(JSON.stringify(systemStatus), {
+    status: 200,
+  });
+}
+async function getDatabaseStatus() {
+  const [version, max_connections, current_connections] = await Promise.all([
+    status.databaseVersion(),
+    status.databaseMaxConnections(),
+    status.databaseCurrentConnections(),
+  ]);
+
+  return {
+    version,
+    max_connections,
+    current_connections,
+  };
 }
