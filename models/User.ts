2d7acd60a21a0c120c698b2ded485a859703b3fb
surfacez <sergiobatistajr94@gmail.com>
2023-12-07 15:10:48 -0300

    Refactor User model and add create and findByEmail functions

--- models/User.ts
+++ models/User.ts
@@ -1,10 +1,30 @@
-export default class User {
-  constructor(
-    readonly id: string,
-    readonly name: string,
-    readonly email: string,
-    readonly role: string,
-    readonly status: string,
-    readonly password?: string,
-  ) {}
+import database from "infra/database";
+import { User } from "./definitions";
+
+async function create(input: User) {
+  return (
+    await database.sql(
+      "INSERT INTO portaria.user (id, name, email, password, role, status ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id, name, email, role, status",
+      [
+        input.id,
+        input.name,
+        input.email,
+        input.password,
+        input.role,
+        input.status,
+      ],
+    )
+  ).rows[0];
 }
+
+async function findByEmail(email: string) {
+  const result = (
+    await database.sql("SELECT * FROM portaria.user where email = $1", [email])
+  ).rows[0];
+  return result;
+}
+
+export default Object.freeze({
+  create,
+  findByEmail,
+});
