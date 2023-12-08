10f26591b747fe50503613924aa3cdc4c87c621e
surfacez <sergiobatistajr94@gmail.com>
2023-12-08 15:42:34 -0300

    Add surf module for handling response

--- app/api/v1/login/route.ts
+++ app/api/v1/login/route.ts
@@ -2,6 +2,7 @@ import validator from "models/validator";
 import user from "models/user";
 import Password from "models/password";
 import jwt from "models/jwt";
+import surf from "models/surf";
 export async function POST(req: Request) {
   const loginParsed = await req.json();
   try {
@@ -13,12 +14,7 @@ export async function POST(req: Request) {
       const match = await Password.verify(isUser.password, password);
       if (!match) throw new Error("Invalid credentials");
       const token = jwt.sign({ id: isUser.id, role: isUser.role });
-      return new Response(undefined, {
-        status: 200,
-        headers: new Headers({
-          "Set-Cookie": `auth=${token}; HttpOnly; Secure; SameSite=Strict`,
-        }),
-      });
+      return surf.response(undefined, 200, token);
     }
   } catch (error) {
     if (error instanceof Error) {
