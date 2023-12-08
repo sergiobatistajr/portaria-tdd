1fd2c1324f739ad5c117903265c2e6e581bece11
surfacez <sergiobatistajr94@gmail.com>
2023-12-08 11:59:57 -0300

    Add tests for creating vehicle entries

--- test/units/validator.test.ts
+++ test/units/validator.test.ts
@@ -180,3 +180,167 @@ describe("Validator Login User Tests", () => {
       expect(result.error.issues[0].message).toEqual("Password is weak");
   });
 });
+
+describe("Validator Create Vehicle Entry Tests", () => {
+  it("should return valid input fields for vehicle", () => {
+    const inputFields = {
+      id: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      status: "inside",
+      name: "John Doe",
+      entryDate: new Date(),
+      plate: "FLK5E66",
+      model: "Commander",
+      pax: 2,
+      createdBy: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      observation: "opa",
+    };
+
+    const result = validator.createVehicleEntry(inputFields);
+
+    expect(result.success).toEqual(true);
+    if (result.success) expect(result.data).toEqual(inputFields);
+  });
+  it("should return error for invalid plate", () => {
+    const inputFields = {
+      id: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      status: "inside",
+      name: "John Doe",
+      entryDate: new Date(),
+      plate: "invalid",
+      model: "Commander",
+      pax: 2,
+      createdBy: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      observation: "opa",
+    };
+
+    const result = validator.createVehicleEntry(inputFields);
+
+    expect(result.success).toEqual(false);
+    if (result.success) {
+      return;
+    } else if (result.error)
+      expect(result.error.issues[0].message).toEqual(
+        "A placa deve seguir o formato do Mercosul: ABC1234 ou ABC1D23",
+      );
+  });
+  it("should return error for invalid status", () => {
+    const inputFields = {
+      id: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      status: "dsadsa",
+      name: "John Doe",
+      entryDate: new Date(),
+      plate: "FLK5E66",
+      model: "Commander",
+      pax: 2,
+      createdBy: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      observation: "opa",
+    };
+
+    const result = validator.createVehicleEntry(inputFields);
+
+    expect(result.success).toEqual(false);
+    if (result.success) {
+      return;
+    } else if (result.error)
+      expect(result.error.issues[0].message).toEqual(
+        `Invalid enum value. Expected 'inside' | 'outside' | 'finished', received '${inputFields.status}'`,
+      );
+  });
+  it("should return error for invalid pax", () => {
+    const inputFields = {
+      id: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      status: "inside",
+      name: "John Doe",
+      entryDate: new Date(),
+      plate: "FLK5E66",
+      model: "Commander",
+      pax: "-1",
+      createdBy: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      observation: "opa",
+    };
+
+    const result = validator.createVehicleEntry(inputFields);
+
+    expect(result.success).toEqual(false);
+    if (result.success) {
+      return;
+    } else if (result.error)
+      expect(result.error.issues[0].message).toEqual(
+        "Number must be greater than or equal to 1",
+      );
+  });
+  it("should return error for entryDate seven days ago", () => {
+    const date = new Date();
+    const sevenDaysAgo = date.setDate(date.getDate() - 7);
+    const inputFields = {
+      id: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      status: "inside",
+      name: "John Doe",
+      entryDate: new Date(sevenDaysAgo),
+      plate: "FLK5E66",
+      model: "Commander",
+      pax: 1,
+      createdBy: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      observation: "",
+    };
+
+    const result = validator.createVehicleEntry(inputFields);
+
+    expect(result.success).toEqual(false);
+    if (result.success) {
+      return;
+    } else if (result.error)
+      expect(result.error.issues[0].message).toEqual(
+        "A data de entrada deve estar entre 7 dias atrás e 7 dias à frente",
+      );
+  });
+  it("should return error for entryDate seven days ahead", () => {
+    const date = new Date();
+    const sevenDaysAhead = date.setDate(date.getDate() + 7);
+    const inputFields = {
+      id: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      status: "inside",
+      name: "John Doe",
+      entryDate: new Date(sevenDaysAhead),
+      plate: "FLK5E66",
+      model: "Commander",
+      pax: 1,
+      createdBy: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      observation: "",
+    };
+
+    const result = validator.createVehicleEntry(inputFields);
+
+    expect(result.success).toEqual(false);
+    if (result.success) {
+      return;
+    } else if (result.error)
+      expect(result.error.issues[0].message).toEqual(
+        "A data de entrada deve estar entre 7 dias atrás e 7 dias à frente",
+      );
+  });
+  it("should return error for invalid apartment", () => {
+    const inputFields = {
+      id: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      status: "inside",
+      name: "John Doe",
+      entryDate: new Date(),
+      plate: "FLK5E66",
+      model: "Commander",
+      pax: 1,
+      createdBy: "f42ecddb-f982-4702-b5c0-e1c5b878c80b",
+      apartment: "-100",
+      observation: "",
+    };
+
+    const result = validator.createVehicleEntry(inputFields);
+
+    expect(result.success).toEqual(false);
+    if (result.success) {
+      return;
+    } else if (result.error)
+      expect(result.error.issues[0].message).toEqual(
+        "O número do apartamento não é válido",
+      );
+  });
+});
