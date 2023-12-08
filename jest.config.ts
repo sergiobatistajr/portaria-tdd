9c7f278935e7e2bbb0c856b8ccc21ad1c4553cd5
surfacez <sergiobatistajr94@gmail.com>
2023-12-07 21:22:32 -0300

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,7 @@
-import { compilerOptions } from "./tsconfig.json";
 import type { JestConfigWithTsJest } from "ts-jest";
 
 const jestConfig: JestConfigWithTsJest = {
-  roots: ["<rootDir>"],
-  modulePaths: [compilerOptions.baseUrl],
+  preset: "ts-jest",
 };
 
 export default jestConfig;
