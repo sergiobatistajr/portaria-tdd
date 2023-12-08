eea53ff90d2fb9b9db782781ddc0b7e8749280fb
surfacez <sergiobatistajr94@gmail.com>
2023-12-08 15:04:37 -0300

    Add API test for creating a vehicle

--- test/integration/api/v1/vehicle/post.test.ts
+++ test/integration/api/v1/vehicle/post.test.ts
@@ -1,21 +1,30 @@
+import surf from "../../../../../models/surf";
 import webserver from "../../../../../infra/webserver";
-describe("Api POST for /login endpoint", () => {
-  beforeAll(async () => {});
+import jwt from "../../../../../models/jwt";
+import { randomUUID } from "crypto";
+describe("API for create Vehicle", () => {
+  let token = jwt.sign({ id: randomUUID(), role: "admin" });
+  beforeAll(async () => {
+    //create a user
+    //login a user
+    //set the token
+  });
   afterAll(async () => {});
   it("POST /vehicle should return 200", async () => {
     const vehicle = {
-      name: "jhon doe",
+      name: "John Doe",
       entry_date: new Date(),
+      plate: "FLK5E66",
+      model: "Commander",
+      pax: 2,
+      observation: "opa",
     };
-    const response = await fetch(`${webserver.host}/api/v1/vehicle`, {
-      method: "POST",
-      headers: new Headers({
-        "Content-Type": "application/json",
-      }),
-      body: JSON.stringify(vehicle),
+    const response = await surf.post(`${webserver.host}/api/v1/vehicle`, {
+      token,
+      body: vehicle,
     });
-    const bodyTest = await response.text();
+    const body = await response.json();
+    console.log(body);
     expect(response.status).toEqual(200);
-    expect(bodyTest).toEqual("test");
   });
 });
