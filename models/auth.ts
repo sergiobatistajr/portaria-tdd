bbd3a0a1a7b9a948a902bdbac29afa27ef1f736e
surfacez <sergiobatistajr94@gmail.com>
2023-12-11 14:32:20 -0300

    Add authentication functionality to the application

---
+++ models/auth.ts
@@ -0,0 +1,25 @@
+import jwt from "./jwt";
+import surf from "./surf";
+import user from "./user";
+async function isAuthenticated(req: Request) {
+  const token = surf.getAuthToken(req);
+  return await verifyAuth(token);
+}
+
+async function verifyAuth(token: string) {
+  if (!token) {
+    return false;
+  }
+  const decoded = jwt.verify(token);
+  const isUser = await user.findById(decoded.id);
+  if (!isUser) {
+    return false;
+  }
+  return {
+    id: isUser.id,
+    role: isUser.role,
+  };
+}
+export default Object.freeze({
+  isAuthenticated,
+});
