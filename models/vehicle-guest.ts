7243c29ed66a2cc7b60eb9b7aa745980a04df61e
surfacez <sergiobatistajr94@gmail.com>
2023-12-13 11:09:03 -0300

    Refactor listAllEntry function to support pagination

--- models/vehicle-guest.ts
+++ models/vehicle-guest.ts
@@ -1,13 +1,15 @@
 import database from "infra/database";
 
-const listAllEntry = async () => {
+async function listAllEntry(currentPage = 1, itemsPerPage = 10) {
+  const offset = (currentPage - 1) * itemsPerPage;
   const result = (
     await database.sql(
-      "SELECT id, name, entryDate, NULL AS plate, NULL AS model FROM portaria.guest UNION ALL SELECT id, name, entryDate, plate, model FROM portaria.vehicle",
+      `SELECT id, name, entryDate, NULL AS plate, NULL AS model FROM portaria.guest UNION ALL SELECT id, name, entryDate, plate, model FROM portaria.vehicle ORDER BY entryDate LIMIT $1 OFFSET $2`,
+      [itemsPerPage, offset],
     )
   ).rows;
   return result;
-};
+}
 
 export default Object.freeze({
   listAllEntry,
