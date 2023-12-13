06b420f0a1eef86849572014a9079beeaa4fb135
surfacez <sergiobatistajr94@gmail.com>
2023-12-13 11:49:33 -0300

    Refactor listAllEntry function to include search functionality

--- app/api/v1/listall/route.ts
+++ app/api/v1/listall/route.ts
@@ -1,6 +1,10 @@
 import vehicleGuest from "models/vehicle-guest";
+import { NextRequest } from "next/server";
 
-export async function GET(req: Request) {
-  const all = await vehicleGuest.listAllEntry();
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const query = searchParams.get("query") || "";
+  const currentPage = parseInt(searchParams.get("page")!) || 1;
+  const all = await vehicleGuest.listAllEntry(query, currentPage);
   return new Response(JSON.stringify(all), { status: 200 });
 }
--- models/vehicle-guest.ts
+++ models/vehicle-guest.ts
@@ -1,11 +1,18 @@
 import database from "infra/database";
 
-async function listAllEntry(currentPage = 1, itemsPerPage = 10) {
+async function listAllEntry(
+  searchTerm = "",
+  currentPage = 1,
+  itemsPerPage = 10,
+) {
   const offset = (currentPage - 1) * itemsPerPage;
   const result = (
     await database.sql(
-      `SELECT id, name, entryDate, NULL AS plate, NULL AS model FROM portaria.guest UNION ALL SELECT id, name, entryDate, plate, model FROM portaria.vehicle ORDER BY entryDate LIMIT $1 OFFSET $2`,
-      [itemsPerPage, offset],
+      `SELECT id, name, entryDate, NULL AS plate, NULL AS model FROM portaria.guest WHERE name LIKE $3 OR CAST(entryDate AS TEXT) LIKE $3
+       UNION ALL 
+       SELECT id, name, entryDate, plate, model FROM portaria.vehicle WHERE name ILIKE $3 OR plate ILIKE $3 OR CAST(entryDate AS TEXT) ILIKE $3
+       ORDER BY entryDate LIMIT $1 OFFSET $2`,
+      [itemsPerPage, offset, `%${searchTerm}%`],
     )
   ).rows;
   return result;
