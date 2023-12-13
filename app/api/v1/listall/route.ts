aefdf15170e7c03110ff80ffea7860ac37866294
surfacez <sergiobatistajr94@gmail.com>
2023-12-13 10:42:18 -0300

    Change POST to GET in listall route

--- app/api/v1/listall/route.ts
+++ app/api/v1/listall/route.ts
@@ -1,7 +1,7 @@
 import surf from "models/surf";
 import vehicleGuest from "models/vehicle-guest";
 
-export async function POST(req: Request) {
+export async function GET(req: Request) {
   const all = await vehicleGuest.listAllEntry();
   return surf.response(all, 200);
 }
