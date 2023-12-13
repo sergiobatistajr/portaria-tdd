3af498a86fc75f282f9a33bebd8cfb24ac661713
surfacez <sergiobatistajr94@gmail.com>
2023-12-13 11:01:36 -0300

    Refactor listall route and test

--- test/integration/api/v1/listall/get.test.ts
+++ test/integration/api/v1/listall/get.test.ts
@@ -1,19 +1,6 @@
 import surf from "../../../../../models/surf";
 import webserver from "../../../../../infra/webserver";
 import listallTestHelper from "./listall.test.helper";
-const guest = {
-  name: `John Doe Listall ${Math.random()}`,
-  entry_date: new Date(),
-  observation: "Lista all",
-};
-const vehicle = {
-  name: `John Doe Listall vehicle ${Math.random()}`,
-  entry_date: new Date(),
-  plate: "FLK5E66",
-  model: "Commander",
-  pax: 2,
-  observation: "opa",
-};
 
 describe("Test", () => {
   let token = "";
@@ -21,22 +8,48 @@ describe("Test", () => {
     await listallTestHelper.createUser();
     const res = await listallTestHelper.loginUser();
     token = res.headers.get("Set-Cookie")?.split("=")[1].split(";")[0]!;
-    await Promise.all([
-      listallTestHelper.createGuest(guest, token),
-      listallTestHelper.createVehicle(vehicle, token),
-    ]);
+
+    const createGuestsAndVehicles = [];
+    for (let i = 10; i < 60; i++) {
+      const guest = {
+        name: `John Doe Listall ${i}`,
+        entry_date: new Date(),
+        observation: "Lista all",
+      };
+      const vehicle = {
+        name: `John Doe Listall vehicle ${i}`,
+        entry_date: new Date(),
+        plate: `FLK5E${i}`,
+        model: "Commander",
+        pax: 2,
+        observation: "opa",
+      };
+      createGuestsAndVehicles.push(listallTestHelper.createGuest(guest, token));
+      createGuestsAndVehicles.push(
+        listallTestHelper.createVehicle(vehicle, token),
+      );
+    }
+    await Promise.all(createGuestsAndVehicles);
   });
+
   afterAll(async () => {
-    await Promise.all([
-      listallTestHelper.deleteGuest(guest.name),
-      listallTestHelper.deleteVehicle(vehicle.plate),
-      listallTestHelper.deleteUser(),
-    ]);
+    const deleteGuestsAndVehicles = [];
+    for (let i = 10; i < 60; i++) {
+      deleteGuestsAndVehicles.push(
+        listallTestHelper.deleteGuest(`John Doe Listall ${i}`),
+      );
+      deleteGuestsAndVehicles.push(
+        listallTestHelper.deleteVehicle(`FLK5E${i}`),
+      );
+    }
+    await Promise.all(deleteGuestsAndVehicles);
+    await listallTestHelper.deleteUser();
   });
+
   it("lista all", async () => {
     const res = await surf.get(`${webserver.host}/api/v1/listall`, {});
-    const text = await res.text();
-
-    console.log(text);
+    const body = await res.json();
+    console.log(body);
+    expect(body.length).toEqual(10);
   });
 });
