02d832d49b214f368b3a54accfca69130a96f874
surfacez <sergiobatistajr94@gmail.com>
2023-12-13 10:42:26 -0300

    Add listall test helper and test case

--- test/integration/api/v1/listall/get.test.ts
+++ test/integration/api/v1/listall/get.test.ts
@@ -1,6 +1,38 @@
 import surf from "../../../../../models/surf";
 import webserver from "../../../../../infra/webserver";
+import listallTestHelper from "./listall.test.helper";
+const guest = {
+  name: `John Doe Listall ${Math.random()}`,
+  entry_date: new Date(),
+  observation: "Lista all",
+};
+const vehicle = {
+  name: `John Doe Listall vehicle ${Math.random()}`,
+  entry_date: new Date(),
+  plate: "FLK5E66",
+  model: "Commander",
+  pax: 2,
+  observation: "opa",
+};
+
 describe("Test", () => {
+  let token = "";
+  beforeAll(async () => {
+    await listallTestHelper.createUser();
+    const res = await listallTestHelper.loginUser();
+    token = res.headers.get("Set-Cookie")?.split("=")[1].split(";")[0]!;
+    await Promise.all([
+      listallTestHelper.createGuest(guest, token),
+      listallTestHelper.createVehicle(vehicle, token),
+    ]);
+  });
+  afterAll(async () => {
+    await Promise.all([
+      listallTestHelper.deleteGuest(guest.name),
+      listallTestHelper.deleteVehicle(vehicle.plate),
+      listallTestHelper.deleteUser(),
+    ]);
+  });
   it("lista all", async () => {
     const res = await surf.get(`${webserver.host}/api/v1/listall`, {});
     const text = await res.text();
---
+++ test/integration/api/v1/listall/listall.test.helper.ts
@@ -0,0 +1,56 @@
+import webserver from "../../../../../infra/webserver";
+import surf from "../../../../../models/surf";
+
+const user = {
+  name: "John Doe",
+  role: "admin",
+  email: "johndoe@listvehicle.com",
+  password: "123456789",
+  confirm_password: "123456789",
+};
+function createUser() {
+  return surf.post(`${webserver.host}/api/v1/test`, { body: user });
+}
+function loginUser() {
+  const user = {
+    email: "johndoe@listvehicle.com",
+    password: "123456789",
+  };
+  return surf.post(`${webserver.host}/api/v1/login`, { body: user });
+}
+function deleteUser() {
+  return fetch(`${webserver.host}/api/v1/test?email=${user.email}`, {
+    method: "DELETE",
+  });
+}
+function deleteVehicle(plate: string = "") {
+  return fetch(`${webserver.host}/api/v1/vehicle?plate=${plate}`, {
+    method: "DELETE",
+  });
+}
+function createVehicle(vehicle: any, token: string) {
+  return surf.post(`${webserver.host}/api/v1/vehicle`, {
+    body: vehicle,
+    token,
+  });
+}
+function createGuest(guest: any, token: string) {
+  return surf.post(`${webserver.host}/api/v1/guest`, {
+    body: guest,
+    token,
+  });
+}
+function deleteGuest(name: string = "") {
+  return fetch(`${webserver.host}/api/v1/guest?name=${name}`, {
+    method: "DELETE",
+  });
+}
+export default Object.freeze({
+  createVehicle,
+  createUser,
+  createGuest,
+  loginUser,
+  deleteUser,
+  deleteVehicle,
+  deleteGuest,
+});
