0a3d2885c4ef8aed3aa7505731fd377783e66340
surfacez <sergiobatistajr94@gmail.com>
2023-12-14 17:54:44 -0300

    Refactor login function and add async/await support

--- test/integration/api/v1/listall/listall.test.helper.ts
+++ test/integration/api/v1/listall/listall.test.helper.ts
@@ -8,43 +8,55 @@ const user = {
   password: "123456789",
   confirm_password: "123456789",
 };
+
 function createUser() {
   return surf.post(`${webserver.host}/api/v1/test`, { body: user });
 }
-function loginUser() {
+
+async function loginUser() {
   const user = {
     email: "johndoe@listvehicle.com",
     password: "123456789",
   };
-  return surf.post(`${webserver.host}/api/v1/login`, { body: user });
+  const res = await surf.post(`${webserver.host}/api/v1/login`, { body: user });
+  const token = surf.getAuthToken(res);
+  return {
+    status: res.status,
+    token,
+  };
 }
 function deleteUser() {
   return fetch(`${webserver.host}/api/v1/test?email=${user.email}`, {
     method: "DELETE",
   });
 }
+
 function deleteVehicle(plate: string = "") {
   return fetch(`${webserver.host}/api/v1/vehicle?plate=${plate}`, {
     method: "DELETE",
   });
 }
+
 function createVehicle(vehicle: any, token: string) {
   return surf.post(`${webserver.host}/api/v1/vehicle`, {
     body: vehicle,
     token,
   });
 }
+
 function createGuest(guest: any, token: string) {
   return surf.post(`${webserver.host}/api/v1/guest`, {
     body: guest,
     token,
   });
 }
+
 function deleteGuest(name: string = "") {
   return fetch(`${webserver.host}/api/v1/guest?name=${name}`, {
     method: "DELETE",
   });
 }
+
 export default Object.freeze({
   createVehicle,
   createUser,
