afad52769ec0a17e9b60588da959462390a52fdb
surfacez <sergiobatistajr94@gmail.com>
2023-12-15 12:51:18 -0300

    Refactor listAll route to improve JSON formatting

--- app/api/v1/listall/route.ts
+++ app/api/v1/listall/route.ts
@@ -5,20 +5,44 @@ import { NextRequest } from "next/server";
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
   const query = searchParams.get("query") || "";
-  const currentPage = parseInt(searchParams.get("page")!) || 1;
-  const guestsAndVehicles = await vehicleGuest.listAllEntry(query, currentPage);
-  const json = jsonFormater(guestsAndVehicles);
-  return surf.response(json, {
+  const currentPage = Number(searchParams.get("page")) || 1;
+  const { guests, totalPages } = await vehicleGuest.listAllEntry(
+    query,
+    currentPage,
+  );
+  const resJSON = jsonFormater({
+    guests,
+    totalPages,
+  });
+  return surf.response(resJSON, {
     status: 200,
   });
 }
-
-function jsonFormater(data: any[]) {
-  return data.map((item) => ({
-    id: item.id as string,
-    name: item.name as string,
-    entry_date: item.entrydate as string,
-    plate: item.plate as string,
-    model: item.model as string,
+function jsonFormater({ guests, totalPages }: JSONInput): JSONOutput {
+  const guestsJson = guests.map((guest) => ({
+    id: guest.id,
+    name: guest.name,
+    entry_date: guest.entrydate,
+    plate: guest.plate,
+    model: guest.model,
   }));
+
+  return {
+    guests: guestsJson,
+    total_pages: totalPages || 0,
+  };
 }
+type JSONInput = {
+  guests: any[];
+  totalPages: number | null;
+};
+type JSONOutput = {
+  guests: {
+    id: string;
+    name: string;
+    entry_date: string | null;
+    plate: string | null;
+    model: string | null;
+  }[];
+  total_pages: number;
+};
