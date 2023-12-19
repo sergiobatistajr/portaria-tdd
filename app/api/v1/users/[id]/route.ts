b4ff95d04f45d2dc256ab9a6c84fb2e3d675d198
surfacez <sergiobatistajr94@gmail.com>
2023-12-19 12:51:34 -0300

    Refactor user route and add validation for user update

--- app/api/v1/users/[id]/route.ts
+++ app/api/v1/users/[id]/route.ts
@@ -1,21 +1,33 @@
 import auth from "models/auth";
 import surf from "models/surf";
 import user from "models/user";
-export function GET(req: Request, { params }: { params: { id: string } }) {
+import validator from "models/validator";
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  const isAuthenticated = await auth.isAuthenticated(req);
+  if (!isAuthenticated) {
+    return surf.response("Não autorizado!", {
+      status: 401,
+    });
+  }
   return surf.response({ eae: params.id }, { status: 200 });
 }
 export async function PATCH(
   req: Request,
   { params }: { params: { id: string } },
 ) {
-  try {
-    const isAuthenticated = await auth.isAuthenticated(req);
-    if (!isAuthenticated) {
-      return surf.response("Não autorizado", {
-        status: 401,
-      });
-    }
-
+  let errors: any[] = [];
+  const isAuthenticated = await auth.isAuthenticated(req);
+  if (!isAuthenticated) {
+    return surf.response("Não autorizado!", {
+      status: 401,
+    });
+  }
+  const body = await req.json();
+  const isUserUpdateValid = validator.userUpdate(body);
+  if (isUserUpdateValid.success) {
     const isUser = await user.findById(params.id);
     if (!isUser) {
       return surf.response("Não existe", { status: 401 });
@@ -23,18 +35,15 @@ export async function PATCH(
     const { name, email, role, status } = await req.json();
     await user.update({ id: params.id, name, email, role, status });
     return surf.response({ id: params.id }, { status: 200 });
-  } catch (error) {
-    if (error instanceof Error) {
-      return surf.response(
-        {
-          error: {
-            message: error.message,
-          },
+  } else {
+    isUserUpdateValid.error.issues.map((error) => errors.push(error.message));
+    return surf.response(
+      {
+        error: {
+          message: errors.join(", "),
         },
-        {
-          status: 401,
-        },
-      );
-    }
+      },
+      { status: 401 },
+    );
   }
 }
--- models/auth.ts
+++ models/auth.ts
@@ -3,13 +3,13 @@ import surf from "./surf";
 import user from "./user";
 async function isAuthenticated(req: Request) {
   const token = surf.getAuthToken(req);
-  return await verifyAuth(token);
-}
-
-async function verifyAuth(token?: string) {
   if (!token) {
     return false;
   }
+  return await verifyAuth(token);
+}
+
+async function verifyAuth(token: string) {
   const decoded = jwt.verify(token);
   const isUser = await user.findById(decoded.id);
   if (!isUser) {
